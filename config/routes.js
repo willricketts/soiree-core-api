66f0ee647bbcb542a1c96f38f4ea3018f4889b7c
Will Ricketts <will@willricketts.com>
2015-01-26 20:39:14 -0500

    added invitation routing

--- config/routes.js
+++ config/routes.js
@@ -15,9 +15,14 @@ module.exports.routes = {
   'get /events/:id' : 'EventController.show',
   'post /events/create' : 'EventController.create',
   'put /events/update/:id' : 'EventController.modify',
-  'delete /events/delete/:id' : 'EventController.deactivate'
+  'delete /events/delete/:id' : 'EventController.deactivate',
 
   // INVITATION
+  'get /invitations' : 'InvitationController.index',
+  'get /invitations/:id' : 'InvitationController.show',
+  'post /invitations/create' : 'InvitationController.create',
+  'put /invitations/update/:id' : 'InvitationController.modify',
+  'delete /invitations/delete/:id' : 'InvitationController.deactivate'
 
   // PLACE
 

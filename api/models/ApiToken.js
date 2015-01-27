d8e4887c287f7708b024da2d4d712e765b76c318
Will Ricketts <will@willricketts.com>
2015-01-26 19:49:55 -0500

    added apiToken model and controller

---
+++ api/models/ApiToken.js
@@ -0,0 +1,29 @@
+/**
+* ApiToken.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  schema: true,
+  attributes: {
+
+    apitoken: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+
+    expiry: {
+      type: 'string',
+      required: true
+    },
+
+    user: {
+      type: 'string',
+      required: true
+    }
+  }
+};

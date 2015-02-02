b84b19e177c7f64720e76f00140a8c24c5562cd3
Will Ricketts <will@willricketts.com>
2015-02-02 14:26:25 -0500

    added action scaffolds for auth

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,20 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	login: login,
+  logout: logout
+};
+
+function login(req, res) {
+
+}
+
+function logout(req, res) {
+  
+}
+
